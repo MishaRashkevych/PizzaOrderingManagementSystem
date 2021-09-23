662b6c77a924926b50c2fa69f850d78f706f44ba
BALASUBRAMANIAN7 <65450643+BALASUBRAMANIAN7@users.noreply.github.com>
2021-09-23 17:53:20 +0530

    Add files via upload

---
+++ PizzaOrderingManagementSystem/Models/OrderDetails.cs
@@ -0,0 +1,15 @@
+ public partial class OrderDetail
+    {
+        public OrderDetail()
+        {
+            OrderItemDetails = new HashSet<OrderItemDetail>();
+        }
+
+        public int Id { get; set; }
+        public int? OrderId { get; set; }
+        public int? PizzaId { get; set; }
+
+        public virtual Order Order { get; set; }
+        public virtual Pizza Pizza { get; set; }
+        public virtual ICollection<OrderItemDetail> OrderItemDetails { get; set; }
+    }
