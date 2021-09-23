c3e6d84771bf8fe538a14730e06d341172760f11
Taras Senkiv <ts.canonir7@gmail.com>
2021-09-23 11:39:48 +0300

    Add repository implementation

---
+++ PizzaOrderingManagementSystem/Services/IRepo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PizzaOrderingManagementSystem.Services
+{
+    interface IRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> Create(TEntity item);
+        TEntity FindById(int id);
+        IEnumerable<TEntity> Get();
+        IEnumerable<TEntity> Get(Func<TEntity, bool> predicate);
+        void Remove(TEntity item);
+        void Update(TEntity item);
+    }
+}
---
+++ PizzaOrderingManagementSystem/Services/Repository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaOrderingManagementSystem.Services
+{
+    class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        PizzaContext _context;
+        DbSet<TEntity> _dbSet;
+
+        public Repository()
+        {
+            _context = new PizzaContext();
+            _dbSet = _context.Set<TEntity>();
+        }
+
+        public IEnumerable<TEntity> Get()
+        {
+            return _dbSet.AsNoTracking().ToList();
+        }
+
+        public IEnumerable<TEntity> Get(Func<TEntity, bool> predicate)
+        {
+            return _dbSet.AsNoTracking().Where(predicate).ToList();
+        }
+
+        public TEntity FindById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public async Task<TEntity> Create(TEntity item)
+        {
+            var entry = await _dbSet.AddAsync(item);
+            _context.SaveChanges();
+            return entry.Entity;
+        }
+
+        public void Update(TEntity item)
+        {
+            _context.Entry(item).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Remove(TEntity item)
+        {
+            _dbSet.Remove(item);
+            _context.SaveChanges();
+        }
+    }
+}
